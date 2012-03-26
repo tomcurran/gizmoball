21976ea0450b6f5105c8d78470778e383cc520cb
Stewart MacKenzie-Leigh <stewartml@gmail.com>
2012-03-26 22:31:17 +0100

    Forgot addremove again.  Also changed the CircleBumperPainter a bit so that it would draw AcceleratorGizmos in a different colour.

--- Gizmoball/src/view/board/CircleBumperPainter.java
+++ Gizmoball/src/view/board/CircleBumperPainter.java
@@ -4,19 +4,34 @@ import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.geom.Ellipse2D;
 
+import model.GizmoType;
 import model.gizmos.IGizmo;
 
 public class CircleBumperPainter implements IGizmoPainter
-{
+{	
 	@Override
 	public void paint(Graphics2D g, IGizmo gizmo)
 	{
 		Ellipse2D.Double circle = new Ellipse2D.Double(gizmo.getX(), gizmo.getY(), gizmo.getWidth(), gizmo.getHeight());
 		
-		g.setColor(Color.GREEN);
+		Color colour;
+		
+		switch (gizmo.getType())
+		{
+			case CircleBumper:
+				colour = Color.GREEN;
+				break;
+				
+			//case AcceleratorGizmo:
+			default:
+				colour = Color.CYAN;
+				break;
+		}
+		
+		g.setColor(colour);
 		g.fill(circle);
 		
-		g.setColor(Color.GREEN.darker());
+		g.setColor(colour.darker());
 		g.draw(circle);
 	}
 }
