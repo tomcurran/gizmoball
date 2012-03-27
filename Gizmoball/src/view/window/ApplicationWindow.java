01ab20c2c7204dac054afea5d0a43ff48763e5af
Andrew White <scurious.aw@gmail.com>
2012-03-27 20:23:50 +0100

    Formatted and commented the window classes.

--- Gizmoball/src/view/window/ApplicationWindow.java
+++ Gizmoball/src/view/window/ApplicationWindow.java
@@ -32,10 +32,9 @@ import exceptions.BadFileException;
  * Main application window.
  */
 @SuppressWarnings("serial")
-public class ApplicationWindow extends JFrame implements Observer
-{
+public class ApplicationWindow extends JFrame implements Observer {
 	public static final int L = 20;
-	
+
 	private GizmoballViewModel viewmodel;
 	private DesignModeViewModel designmodeViewmodel;
 
@@ -44,154 +43,148 @@ public class ApplicationWindow extends JFrame implements Observer
 	private ToolbarButtonArea toolbar;
 	private JPanel contentPane;
 	private JLabel statusBar;
-	
-	public ApplicationWindow()
-	{
+
+	/**
+	 * Creates a new window for gizmoball. 
+	 */
+	public ApplicationWindow() {
 		super("Gizmoball");
 		super.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
+
 		viewmodel = new GizmoballViewModel();
 		viewmodel.addObserver(this);
-		
-		designmodeViewmodel = new DesignModeViewModel(viewmodel.getBoard(), viewmodel.getTriggerHandler());
+
+		designmodeViewmodel = new DesignModeViewModel(viewmodel.getBoard(),
+				viewmodel.getTriggerHandler());
 		designmodeViewmodel.addObserver(this);
-		
+
 		initialiseComponents();
 		initialiseActionListeners();
 	}
-	
-	
-	private void initialiseComponents()
-	{
+
+	/**
+	 * Initialise all the window components. 
+	 */
+	private void initialiseComponents() {
 		toolbar = new ToolbarButtonArea(viewmodel, designmodeViewmodel);
 		boardView = new AnimationPanel(viewmodel, designmodeViewmodel);
 
 		JMenuBar menubar = new JMenuBar();
 		super.setJMenuBar(menubar);
-		
+
 		JMenu fileMenu = new JMenu("File");
 		menubar.add(fileMenu);
-		
+
 		newMenuItem = new JMenuItem("New");
 		fileMenu.add(newMenuItem);
-		
+
 		openMenuItem = new JMenuItem("Open");
 		fileMenu.add(openMenuItem);
-		
+
 		saveMenuItem = new JMenuItem("Save");
 		fileMenu.add(saveMenuItem);
-		
+
 		statusBar = new JLabel("Status.");
 		statusBar.setBorder(new EmptyBorder(5, 5, 5, 5));
-		
+
 		contentPane = new JPanel();
 		contentPane.setLayout(new BorderLayout());
 		contentPane.setPreferredSize(new Dimension(500, 410));
 		contentPane.add(boardView, BorderLayout.CENTER);
 		contentPane.add(toolbar, BorderLayout.EAST);
 		contentPane.add(statusBar, BorderLayout.SOUTH);
-		
+
 		super.setContentPane(contentPane);
 		super.setMinimumSize(new Dimension(650, 580));
-		
+
 		super.pack();
 		super.requestFocus();
 	}
-	
-	
-	private void initialiseActionListeners()
-	{
+
+	/**
+	 * Sets action listeners. 
+	 */
+	private void initialiseActionListeners() {
 		final JFrame parent = this;
-		
-		newMenuItem.addActionListener(new ActionListener()
-		{
+
+		newMenuItem.addActionListener(new ActionListener() {
 			@Override
-			public void actionPerformed(ActionEvent e)
-			{
+			public void actionPerformed(ActionEvent e) {
 				viewmodel.newGame();
 			}
 		});
-		
-		openMenuItem.addActionListener(new ActionListener()
-		{
+
+		openMenuItem.addActionListener(new ActionListener() {
 			@Override
-			public void actionPerformed(ActionEvent e)
-			{
-				JFileChooser chooser = new JFileChooser(System.getProperty("user.dir"));
-				
-				if (chooser.showOpenDialog(parent) == JFileChooser.APPROVE_OPTION)
-				{
+			public void actionPerformed(ActionEvent e) {
+				JFileChooser chooser = new JFileChooser(System
+						.getProperty("user.dir"));
+
+				if (chooser.showOpenDialog(parent) == JFileChooser.APPROVE_OPTION) {
 					File file = chooser.getSelectedFile();
-					
-					try
-					{
+
+					try {
 						viewmodel.loadGame(file.getAbsolutePath());
-					}
-					catch (FileNotFoundException ex)
-					{
-						JOptionPane.showMessageDialog(parent, "The file cannot be found.", "Load error", JOptionPane.ERROR_MESSAGE);
-					}
-					catch (IOException ex)
-					{
-						JOptionPane.showMessageDialog(parent, "Error reading file: " + ex.getMessage(), "Load error", JOptionPane.ERROR_MESSAGE);
-					}
-					catch (BadFileException ex)
-					{
-						JOptionPane.showMessageDialog(parent, "The file format is incorrect: " + ex.getMessage(), "Load error", JOptionPane.ERROR_MESSAGE);
+					} catch (FileNotFoundException ex) {
+						JOptionPane.showMessageDialog(parent,
+								"The file cannot be found.", "Load error",
+								JOptionPane.ERROR_MESSAGE);
+					} catch (IOException ex) {
+						JOptionPane.showMessageDialog(parent,
+								"Error reading file: " + ex.getMessage(),
+								"Load error", JOptionPane.ERROR_MESSAGE);
+					} catch (BadFileException ex) {
+						JOptionPane.showMessageDialog(
+								parent,
+								"The file format is incorrect: "
+										+ ex.getMessage(), "Load error",
+								JOptionPane.ERROR_MESSAGE);
 					}
 				}
 			}
 		});
-		
-		saveMenuItem.addActionListener(new ActionListener()
-		{
+
+		saveMenuItem.addActionListener(new ActionListener() {
 			@Override
-			public void actionPerformed(ActionEvent e)
-			{
-				JFileChooser chooser = new JFileChooser(System.getProperty("user.dir"));
-				
-				if (chooser.showSaveDialog(parent) == JFileChooser.APPROVE_OPTION)
-				{
+			public void actionPerformed(ActionEvent e) {
+				JFileChooser chooser = new JFileChooser(System
+						.getProperty("user.dir"));
+
+				if (chooser.showSaveDialog(parent) == JFileChooser.APPROVE_OPTION) {
 					File file = chooser.getSelectedFile();
-					
-					try
-					{
+
+					try {
 						viewmodel.saveGame(file.getAbsolutePath());
-					}
-					catch (IOException ex)
-					{
-						JOptionPane.showMessageDialog(parent, "Error writing file: " + ex.getMessage(), "Save error", JOptionPane.ERROR_MESSAGE);
+					} catch (IOException ex) {
+						JOptionPane.showMessageDialog(parent,
+								"Error writing file: " + ex.getMessage(),
+								"Save error", JOptionPane.ERROR_MESSAGE);
 					}
 				}
 			}
 		});
 	}
-	
-	
+
 	@Override
-	public void update(Observable source, Object arg)
-	{
-		UpdateReason reason = (UpdateReason)arg;
-		
-		switch (reason)
-		{
-			case RunStateChanged:
-				toolbar.setRunMode(viewmodel.getIsRunning());
-				
-				if (viewmodel.getIsRunning())
-					contentPane.remove(statusBar);
-				else
-					contentPane.add(statusBar, BorderLayout.SOUTH);
-					
-				break;
-				
-			case StatusChanged:
-				statusBar.setText(designmodeViewmodel.getStatusMessage());
-				break;
+	public void update(Observable source, Object arg) {
+		UpdateReason reason = (UpdateReason) arg;
+
+		switch (reason) {
+		case RunStateChanged:
+			toolbar.setRunMode(viewmodel.getIsRunning());
+
+			if (viewmodel.getIsRunning())
+				contentPane.remove(statusBar);
+			else
+				contentPane.add(statusBar, BorderLayout.SOUTH);
+
+			break;
+
+		case StatusChanged:
+			statusBar.setText(designmodeViewmodel.getStatusMessage());
+			break;
 		}
 	}
-	
-
 
 	private KeyListener linkListener;
 
--- Gizmoball/src/view/window/ApplicationWindow2.java
+++ Gizmoball/src/view/window/ApplicationWindow2.java
@@ -32,10 +32,9 @@ import exceptions.BadFileException;
  * Main application window.
  */
 @SuppressWarnings("serial")
-public class ApplicationWindow2 extends JFrame implements Observer
-{
+public class ApplicationWindow2 extends JFrame implements Observer {
 	public static final int L = 20;
-	
+
 	private GizmoballViewModel viewmodel;
 	private DesignModeViewModel designmodeViewmodel;
 
@@ -44,154 +43,145 @@ public class ApplicationWindow2 extends JFrame implements Observer
 	private ToolbarButtonArea toolbar;
 	private JPanel contentPane;
 	private JLabel statusBar;
-	
-	public ApplicationWindow2()
-	{
+
+	/**
+	 * Creates a new window for gizmoball. 
+	 */
+	public ApplicationWindow2() {
 		super("Gizmoball");
 		super.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
+
 		viewmodel = new GizmoballViewModel();
 		viewmodel.addObserver(this);
-		
+
 		designmodeViewmodel = new DesignModeViewModel(viewmodel.getBoard(), viewmodel.getTriggerHandler());
 		designmodeViewmodel.addObserver(this);
-		
+
 		initialiseComponents();
 		initialiseActionListeners();
 	}
-	
-	
-	private void initialiseComponents()
-	{
+
+	/**
+	 * Initialise all the window components. 
+	 */
+	private void initialiseComponents() {
 		toolbar = new ToolbarButtonArea(viewmodel, designmodeViewmodel);
 		boardView = new AnimationPanel(viewmodel, designmodeViewmodel);
 
 		JMenuBar menubar = new JMenuBar();
 		super.setJMenuBar(menubar);
-		
+
 		JMenu fileMenu = new JMenu("File");
 		menubar.add(fileMenu);
-		
+
 		newMenuItem = new JMenuItem("New");
 		fileMenu.add(newMenuItem);
-		
+
 		openMenuItem = new JMenuItem("Open");
 		fileMenu.add(openMenuItem);
-		
+
 		saveMenuItem = new JMenuItem("Save");
 		fileMenu.add(saveMenuItem);
-		
+
 		statusBar = new JLabel("Status.");
 		statusBar.setBorder(new EmptyBorder(5, 5, 5, 5));
-		
+
 		contentPane = new JPanel();
 		contentPane.setLayout(new BorderLayout());
 		contentPane.setPreferredSize(new Dimension(500, 500));
 		contentPane.add(boardView, BorderLayout.CENTER);
 		contentPane.add(toolbar, BorderLayout.EAST);
 		contentPane.add(statusBar, BorderLayout.SOUTH);
-		
+
 		super.setContentPane(contentPane);
 		super.setMinimumSize(new Dimension(500, 500));
-		
+
 		super.pack();
 		super.requestFocus();
 	}
-	
-	
-	private void initialiseActionListeners()
-	{
+
+	/**
+	 * Sets the actionlisteners. 
+	 */
+	private void initialiseActionListeners() {
 		final JFrame parent = this;
-		
-		newMenuItem.addActionListener(new ActionListener()
-		{
+
+		newMenuItem.addActionListener(new ActionListener() {
 			@Override
-			public void actionPerformed(ActionEvent e)
-			{
+			public void actionPerformed(ActionEvent e) {
 				viewmodel.newGame();
 			}
 		});
-		
-		openMenuItem.addActionListener(new ActionListener()
-		{
+
+		openMenuItem.addActionListener(new ActionListener() {
 			@Override
-			public void actionPerformed(ActionEvent e)
-			{
-				JFileChooser chooser = new JFileChooser(System.getProperty("user.dir"));
-				
-				if (chooser.showOpenDialog(chooser) == JFileChooser.APPROVE_OPTION)
-				{
+			public void actionPerformed(ActionEvent e) {
+				JFileChooser chooser = new JFileChooser(System
+						.getProperty("user.dir"));
+
+				if (chooser.showOpenDialog(chooser) == JFileChooser.APPROVE_OPTION) {
 					File file = chooser.getSelectedFile();
-					
-					try
-					{
+
+					try {
 						viewmodel.loadGame(file.getAbsolutePath());
-					}
-					catch (FileNotFoundException ex)
-					{
-						JOptionPane.showMessageDialog(parent, "The file cannot be found.", "Load error", JOptionPane.ERROR);
-					}
-					catch (IOException ex)
-					{
-						JOptionPane.showMessageDialog(parent, "Error reading file: " + ex.getMessage(), "Load error", JOptionPane.ERROR);
-					}
-					catch (BadFileException ex)
-					{
-						JOptionPane.showMessageDialog(parent, "The file format is incorrect.", "Load error", JOptionPane.ERROR);
+					} catch (FileNotFoundException ex) {
+						JOptionPane.showMessageDialog(parent,
+								"The file cannot be found.", "Load error",
+								JOptionPane.ERROR);
+					} catch (IOException ex) {
+						JOptionPane.showMessageDialog(parent,
+								"Error reading file: " + ex.getMessage(),
+								"Load error", JOptionPane.ERROR);
+					} catch (BadFileException ex) {
+						JOptionPane.showMessageDialog(parent,
+								"The file format is incorrect.", "Load error",
+								JOptionPane.ERROR);
 					}
 				}
 			}
 		});
-		
-		saveMenuItem.addActionListener(new ActionListener()
-		{
-			@Override
-			public void actionPerformed(ActionEvent e)
-			{
-				JFileChooser chooser = new JFileChooser(System.getProperty("user.dir"));
 
-				if (chooser.showSaveDialog(chooser) == JFileChooser.APPROVE_OPTION)
-				{
+		saveMenuItem.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				JFileChooser chooser = new JFileChooser(System
+						.getProperty("user.dir"));
+
+				if (chooser.showSaveDialog(chooser) == JFileChooser.APPROVE_OPTION) {
 					File file = chooser.getSelectedFile();
 
-					try
-					{
+					try {
 						viewmodel.saveGame(file.getAbsolutePath());
-					}
-					catch (IOException ex)
-					{
-						JOptionPane.showMessageDialog(parent, "Error saving file: " + ex.getMessage(), "Save error", JOptionPane.ERROR);
+					} catch (IOException ex) {
+						JOptionPane.showMessageDialog(parent,
+								"Error saving file: " + ex.getMessage(),
+								"Save error", JOptionPane.ERROR);
 					}
 				}
 			}
 		});
 	}
-	
-	
+
 	@Override
-	public void update(Observable source, Object arg)
-	{
-		UpdateReason reason = (UpdateReason)arg;
-		
-		switch (reason)
-		{
-			case RunStateChanged:
-				toolbar.setRunMode(viewmodel.getIsRunning());
-				
-				if (viewmodel.getIsRunning())
-					contentPane.remove(statusBar);
-				else
-					contentPane.add(statusBar, BorderLayout.SOUTH);
-					
-				break;
-				
-			case StatusChanged:
-				statusBar.setText(designmodeViewmodel.getStatusMessage());
-				break;
+	public void update(Observable source, Object arg) {
+		UpdateReason reason = (UpdateReason) arg;
+
+		switch (reason) {
+		case RunStateChanged:
+			toolbar.setRunMode(viewmodel.getIsRunning());
+
+			if (viewmodel.getIsRunning())
+				contentPane.remove(statusBar);
+			else
+				contentPane.add(statusBar, BorderLayout.SOUTH);
+
+			break;
+
+		case StatusChanged:
+			statusBar.setText(designmodeViewmodel.getStatusMessage());
+			break;
 		}
 	}
-	
-
 
 	private KeyListener linkListener;
 
